9b4b264b8fad6012abc6eaa6f9515ce4c34db498
JMariam <mariamjubril29@gmail.com>
2025-01-28 22:02:51 +0100

    fifth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/Components/Layout";
+import Projects from "@/Components/Projects";
 
 export default function Home() {
   return (
@@ -24,31 +25,11 @@ export default function Home() {
             eligendi quasi. Placeat ex, quidem est facere nisi suscipit earum
             odio aut alias iusto, qui fugit totam repellendus praesentium at
             tempore unde harum et fugiat dolore. Obcaecati commodi harum totam
-            quaerat? 
+            quaerat?
           </p>
         </section>
         <section id="project" className="">
-          <p className="text-[14px] leading-[24px] ">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore,
-            neque? Nemo, eius! Repellendus quidem eveniet, quo iure laudantium
-            labore soluta distinctio modi non est corrupti aspernatur molestiae
-            quia perferendis minus libero aut totam, animi doloribus <br />{" "}
-            <br />
-            exercitationem dicta excepturi! Quos quas blanditiis veniam,
-            assumenda quod perferendis, vitae dicta ab laudantium, beatae enim
-            possimus. Delectus debitis modi nostrum, quod voluptas at deleniti
-            iste libero explicabo nisi harum consectetur obcaecati! Fuga
-            assumenda animi nam, qui iusto magni quae atque consequuntur
-            repellendus possimus, debitis a, quidem dicta placeat eveniet sequi?
-            Sint ipsum, nam minima quidem veniam corporis impedit accusamus
-            deserunt maxime rerum voluptates <br /> <br />
-            laboriosam ad qui nulla laudantium totam asperiores mollitia placeat
-            voluptatibus facilis! Assumenda qui fuga dolorem atque optio
-            eligendi quasi. Placeat ex, quidem est facere nisi suscipit earum
-            odio aut alias iusto, qui fugit totam repellendus praesentium at
-            tempore unde harum et fugiat dolore. Obcaecati commodi harum totam
-            quaerat? 
-          </p>
+          <Projects img = '/pro1.png' text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab porriste obcaecati fugiat voluptates repudiandae sapiente voluptatummaxime alias expedita!" />
         </section>
       </div>
     </Layout>
