d8e4162e1dbcbd1dd6f2bfb58162d6cb168020e6
JMariam <mariamjubril29@gmail.com>
2026-01-11 14:54:01 +0100

    vercel fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,11 @@
+export const dynamic = "force-static";
+
 export default function sitemap() {
   return [
     {
-      url: "https://mariamjubril.vercel.app",
-      lastModified: new Date(),
+      url: "https://mariamjubril.vercel.app", // or your custom domain
+      lastModified: "2026-01-11",
     },
   ];
 }
+
